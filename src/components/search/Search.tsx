53000a69ed88cd30b57b0552fff30e58bb537800
anderskambenders <a.zetsmanis@mail.ru>
2024-07-07 13:43:25 +0300

    feat: add search layout

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,8 +1,49 @@
-import { Component, ReactNode } from 'react';
+import { Component } from 'react';
+import './search.css';
 
-class Search extends Component {
-  render(): ReactNode {
-    return <></>;
+interface SearchProps {
+  children?: JSX.Element;
+  updateData?: (value: string) => void;
+}
+type State = {
+  value: string;
+};
+
+class Search extends Component<SearchProps, State> {
+  inputVal: string;
+  constructor(props: SearchProps) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+    this.inputVal = '';
+  }
+
+  render() {
+    return (
+      <>
+        <h2 className="header">Api: Star Wars (SWAPI)</h2>
+        <div className="search__container">
+          <form>
+            <label className="search__label">
+              Enter what you want to see:
+              <input
+                className="search__input"
+                name="key"
+                id="key"
+                type="text"
+                placeholder="enter search param"
+                autoComplete="on"
+                value={this.state.value}
+              />
+            </label>
+            <button className="search__btn" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </>
+    );
   }
 }
 
