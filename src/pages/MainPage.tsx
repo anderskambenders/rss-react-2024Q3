e30fec7bbc451603f0a362b7e57d661af75f2f4b
anderskambenders <a.zetsmanis@mail.ru>
2024-07-07 15:50:43 +0300

    feat: search props to list result

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,12 +2,30 @@ import { Component } from 'react';
 import Search from '../components/search/Search';
 import ListResult from '../components/list-result/ListResult';
 
-export default class MainPage extends Component {
+type State = {
+  searchVal: string;
+};
+
+export default class MainPage extends Component<
+  { children?: JSX.Element },
+  State
+> {
+  constructor(props: { children?: JSX.Element }) {
+    super(props);
+    this.state = {
+      searchVal: '',
+    };
+  }
+
+  updateData = (value: string) => {
+    this.setState({ searchVal: value });
+  };
+
   render() {
     return (
       <>
-        <Search />
-        <ListResult />
+        <Search updateData={this.updateData} />
+        <ListResult data={this.state.searchVal} />
       </>
     );
   }
